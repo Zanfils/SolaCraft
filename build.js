a17d768930a9bcf9cdbec96b26f66249e260413a
Zanfils <Zanfils@users.noreply.github.com>
2021-01-23 23:55:24 +0100

    update build

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'SolaCraft',
+        productName: 'SolaCraft',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2021 Daniel Scalzi & Zanfils',
         directories: {
             buildResources: 'build',
             output: 'dist'
